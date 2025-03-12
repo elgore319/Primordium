da38fcc15255732225fbe0245943e139f4a41bd0
Eddie Gero <elgore319@gmail.com>
2025-03-12 10:10:41 -0500

    Added light texture

--- obj/Debug/net8.0/Primordium.AssemblyInfo.cs
+++ obj/Debug/net8.0/Primordium.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primordium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d4f22444117309809ea08fd0c0cf16896ea9a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a286a5062f0b9d092e241835019e103c90b87910")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primordium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primordium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
